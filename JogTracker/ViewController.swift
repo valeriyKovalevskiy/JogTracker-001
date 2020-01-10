fab27e8e9901e088a52f6664b41ded94259b3e51
Valeriy Kovalevskiy <vkovalevskiy@wakeapp.ru>
2020-01-10 13:41:03 +0300

    Initial Commit

---
+++ JogTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JogTracker
+//
+//  Created by валерий on 1/10/20.
+//  Copyright © 2020 ValeriyKovalevskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
